0f7820d8480f4dd93bf4a7ebc53068f84f090f9e
Sveta Lalayan <lalayan-svetlana@list.ru>
2025-04-21 20:38:44 +0300

    c11

--- config/settings.py
+++ config/settings.py
@@ -83,16 +83,17 @@ if "test" in sys.argv:
             "NAME": BASE_DIR / "test_db/sqlite3",
         }
     }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "POSTGRES_DB": os.getenv("NAME"),
-        "POSTGRES_USER": "postgres",
-        "POSTGRES_HOST": "db",
-        "PORT": "5432",
-        "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "POSTGRES_DB": os.getenv("NAME"),
+            "POSTGRES_USER": "postgres",
+            "POSTGRES_HOST": "db",
+            "PORT": "5432",
+            "POSTGRES_PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        }
     }
-}
 
 
 AUTH_PASSWORD_VALIDATORS = [
